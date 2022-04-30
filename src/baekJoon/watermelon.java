d4f1bb63b276ee4f82dc215b50733209c0def726
kimsanhaa <ttksgk@gmail.com>
2022-04-30 12:56:08 +0900

    프로그래머스 수박수박수박수박 완료

---
+++ src/baekJoon/watermelon.java
@@ -0,0 +1,26 @@
+package baekJoon;
+
+public class watermelon {
+    public String solution(int n) {
+        String answer = "";
+        // n이 2의 배수면 n/2 값 만큼 수박을 반복
+        // n이 2의 배수가 아니면 n/2하고 +수
+        for(int i=1; i<=n/2;i++){
+            answer = answer+"수박";
+        }
+        if(n%2==1){
+            answer= answer+"수";
+        }
+
+
+        System.out.println(answer);
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        watermelon watermelon = new watermelon();
+        watermelon.solution(11);
+
+    }
+}
