656d08956590adba2efe860fab4a9cded131c264
kimsanhaa <ttksgk@gmail.com>
2022-05-08 22:34:06 +0900

    시간복잡도

---
+++ src/doit_java/timeComplexity.java
@@ -0,0 +1,13 @@
+package doit_java;
+
+public class timeComplexity {
+    public static void main(String[] args) {
+
+
+        int n= 1;
+        while(n>0) {
+            double temp = Math.random();
+        System.out.println(temp);
+    }
+    }
+}
