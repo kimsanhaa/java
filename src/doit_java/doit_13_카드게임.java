93008d60dca1f0bc678bcac4a25552063a836c08
kimsanhaa <ttksgk@gmail.com>
2022-05-26 20:20:10 +0900

    doit_14 절대값힙구하기

--- src/doit_java/doit_13_카드게임.java
+++ src/doit_java/doit_13_카드게임.java
@@ -12,26 +12,22 @@ public class doit_13_카드게임 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         Queue<Integer> que = new LinkedList<>();
-        // Que에 넣기
-        for(int i=1; i<=N; i++)que.add(i);
 
-        // while문 언제 까지? 큐에 크기가 1일 때 까지
-        //1.처음 값 빼기
-
-        while(que.size()>1){
-            que.remove();
-
-            int num =que.poll();
-
-            que.add(num);
-
+        for(int i=1; i<=N; i++){
+            que.add(i);
         }
+
+
+        while(que.size()!=1){
+            que.poll();
+            Integer poll = que.poll();
+            que.offer(poll);
+            System.out.println("실행");
+        }
+
         for (Integer integer : que) {
             System.out.println(integer);
-
         }
-
-
 
 
 
--- src/doit_java/doit_14_절대값힙구현하기.java
+++ src/doit_java/doit_14_절대값힙구현하기.java
@@ -3,13 +3,49 @@ package doit_java;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class doit_14_절대값힙구현하기 {
     public static void main(String[] args) throws IOException {
-        //1. 배열에 정수 0이 아닌 x를 넣는다.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        //2. 배열에서 절댓값이 가장 작은 값을 출력하고  그값을 제거, 절댓값이 가장 작은 값이 여러개일 때는, 가장 작은 수를 출력하고 , 그값을 배열에서 제거
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        PriorityQueue<Integer> que = new PriorityQueue<>((o1, o2) -> {
+            int firstAbs = Math.abs(o1);
+            int secondAbs = Math.abs(o2);
+            if(firstAbs==secondAbs){
+                return o1>o2 ? 1 :-1;
+            }
+            else {
+                return firstAbs-secondAbs;
+            }
+        });
+
+        for(int i=0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+
+            if(x==0){
+                if(que.isEmpty()){
+                    System.out.println(0);
+                }else{
+                System.out.println(que.poll());
+                //절대 값이 가장 작은 값을 출력
+            }
+            }
+
+            else{
+                que.add(x);
+            }
+
+
+
+        }
+
+
 
 
     }
